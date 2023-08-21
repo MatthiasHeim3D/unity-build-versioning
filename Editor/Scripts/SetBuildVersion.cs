08b3fa4b86abbd9a529b633dabbe8740e7ea0738
Matthias Heim <Matthias.Heim@dlr.de>
2023-08-21 19:12:02 +0200

    Replaced PostProcessBuild with IProcessBuildWithReport

--- Editor/Scripts/SetBuildVersion.cs
+++ Editor/Scripts/SetBuildVersion.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using UnityEditor;
 using UnityEditor.Build;
@@ -6,7 +5,7 @@ using UnityEditor.Build.Reporting;
 using UnityEditor.Callbacks;
 using UnityEngine;
 
-public class SetBuildVersion : IPreprocessBuildWithReport
+public class SetBuildVersion : IPreprocessBuildWithReport, IPostprocessBuildWithReport
 {
     public int callbackOrder { get { return 0; } }
 
@@ -29,15 +28,15 @@ public class SetBuildVersion : IPreprocessBuildWithReport
         }
     }
 
-    [PostProcessBuild]
-    public static void OnPostProcessBuild(BuildTarget target, string pathToExecutable)
+    public void OnPostprocessBuild(BuildReport report)
     {
         if (GetBuildInfo(out BuildInfo buildInfo))
         {
             if (buildInfo.CreateVersionTxtFileNextToExecutable)
             {
-                RemovePreviousVersionTxt(pathToExecutable);
-                CreateCurrentVersionTxt(pathToExecutable, target);
+                string pathToExe = report.summary.outputPath;
+                RemovePreviousVersionTxt(pathToExe);
+                CreateCurrentVersionTxt(pathToExe, report.summary.platform);
             }
         }
     }
@@ -53,14 +52,14 @@ public class SetBuildVersion : IPreprocessBuildWithReport
         }
     }
 
-    private static void CreateCurrentVersionTxt(string pathToExecutable, BuildTarget target)
+    private static void CreateCurrentVersionTxt(string pathToExecutable, BuildTarget targetPlatform)
     {
         if (GetBuildInfo(out BuildInfo buildInfo))
         {
             string directory = Path.GetDirectoryName(pathToExecutable);
             string debugOrRelease = string.Empty;
-            
-            if(buildInfo.AppendDevelopmentOrReleaseToVersion)
+
+            if (buildInfo.AppendDevelopmentOrReleaseToVersion)
                 debugOrRelease = EditorUserBuildSettings.development ? "_development" : "_release";
 
             string currentVersionTxtPath = Path.Combine(directory, $"version_{buildInfo.GetVersionString()}{debugOrRelease}.txt");
@@ -76,6 +75,10 @@ public class SetBuildVersion : IPreprocessBuildWithReport
         if (buildInfoPath.Length > 0)
         {
             buildInfo = AssetDatabase.LoadAssetAtPath<BuildInfo>(AssetDatabase.GUIDToAssetPath(buildInfoPath[0]));
+
+            if (buildInfoPath.Length > 1)
+                Debug.Log("Multiple BuildInfo assets found, using first found.");
+
             return true;
         }
 
